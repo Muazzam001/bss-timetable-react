d387c984f2da144997b8a49e3bf9ec6cc27513da
Muazzam Younus <muazzam.y@retailinsight.io>
2023-09-19 17:38:54 +0500

    Components Alignments

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react({
-        refresh: true,
-        babel: {
-            plugins: ['babel-plugin-macros']
-        },
-    })],
+    plugins: [
+        "react-refresh",
+        react({
+            refresh: true,
+            babel: {
+                plugins: ['babel-plugin-macros']
+            },
+        })
+    ],
+    "rules": {
+        "react-refresh/only-export-components": "warn"
+    },
     resolve: {
         alias: {
             "@": "./",
