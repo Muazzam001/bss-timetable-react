618ee0693add2705b62fe539155098fc616ceefa
Muazzam Younus <muazzam.y@retailinsight.io>
2023-09-30 16:19:02 +0500

    fix: Manage Slot addition

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,11 @@ import {useState} from 'react';
 import {TimetableContext} from "./utils/timetableContext.js";
 
 export default function App() {
-    const [defaultValue, setDefaultVlaue] = useState("");
+    const [defaultValue, setDefaultValue] = useState("");
 
     return (
-        <TimetableContext.Provider value={{ 
-            defaultValue, 
-            setDefaultVlaue
-            }}>
-            <AppRoutes />
+        <TimetableContext.Provider value={{defaultValue, setDefaultValue}}>
+            <AppRoutes/>
         </TimetableContext.Provider>
     );
 }
